a928d434d39abce00c84065f8aceb1b7723d2b69
Анастасия Зимина <Asya.Zimina777@mail.ru>
2021-02-09 21:27:56 +0300

    add main page

--- my-app/src/App/pages/MainPage/MainPage.tsx
+++ my-app/src/App/pages/MainPage/MainPage.tsx
@@ -1,1 +1,27 @@
-export {}
+import {Header} from '../../../components/Header/index';
+import {MainVideo} from "../../../components/MainVideo";
+import './MainPage.css';
+
+const Main = ({items}: any) => {
+    console.log(items);
+    return (
+        <div className="main background">
+            {
+                items.map((item: any) => {
+                    return <MainVideo items={item}></MainVideo>;
+                })
+            }
+        </div>
+    );
+};
+
+
+export const MainPage = (props: any) => {
+
+    return (
+        <div>
+            <Header></Header>
+            <Main items={props.items}></Main>
+        </div>
+    );
+}
---
+++ my-app/src/components/MainVideo/MainVideo.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './MainVideo.css'
+
+export const MainVideo = ({items}: any) => {
+    return (
+        <div className="main-video">
+            <div className="main-image-content">
+                <img className="main-image-content-img" src={items.preview}/>
+                <span className="duration">{items.duration}</span>
+                <span className="views">{items.views}</span>
+            </div>
+            <div className="bottom-info">
+                <div> <img className="channel-logo" src={items.avatar}/> </div>
+                <div className="right-info">
+                    <div className="heading">{items.title}</div>
+                    <div className="low">
+                        <div className="name-channel">{items.nickname}</div> ·
+                        <div className="time"> {items.time}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
